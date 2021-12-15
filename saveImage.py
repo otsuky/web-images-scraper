7ec6be541e5e8656de1bb27c67433ac0191e8b1c
Y.Otsuki <vloxolv.otky.vlowolv@gmail.com>
2021-12-16 02:10:58 +0900

    update save.py

--- saveImage.py
+++ saveImage.py
@@ -1,44 +1,49 @@
 import os
 import time
+import pathlib
 import urllib.error
 import urllib.request
+
 
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse
 
-url = "https://news.yahoo.co.jp/list/"
-download_dir = "data"
 sleep_time_sec = 100 / 1000
 
 
-req = urllib.request.Request(url)
-html = urllib.request.urlopen(req)
+def download_file_to_dir(url, download_dir):
+    download_path = pathlib.Path(download_dir + os.path.dirname(urlparse(url).path))
+    download_path.mkdir(parents=True, exist_ok=True)
 
-soup = BeautifulSoup(html, "html.parser")
-
-src_list = [img.get("src") for img in soup.find_all("img")]
-
-
-def download_file(url, dst_path):
     try:
         with urllib.request.urlopen(url) as web_file, open(
-            dst_path, "wb"
+            os.path.join(download_path, os.path.basename(url)), "wb"
         ) as local_file:
             local_file.write(web_file.read())
+            print("✅ Saved")
     except urllib.error.URLError as e:
         print(e)
+        print("❗️Error")
+    except OSError as e:
+        print(e)
+        print("❗️Error")
 
 
-def download_file_to_dir(url, dst_dir):
-    download_file(url, os.path.join(dst_dir, os.path.basename(url)))
-
-
-def download_images_from_src_list(src_list, dst_dir):
+def download_images_from_src_list(src_list, download_dir):
     for url in src_list:
         file_path = urljoin(url, urlparse(url).path)
         print(file_path)
-        # download_file_to_dir(file_path, dst_dir)
+        download_file_to_dir(file_path, download_dir)
         time.sleep(sleep_time_sec)
 
 
-download_images_from_src_list(src_list, download_dir)
+def download_images_from_url_list(url_list, download_dir):
+    for url in url_list:
+        print("\033[32m" + url + "\033[0m")
+        req = urllib.request.Request(url)
+        html = urllib.request.urlopen(req)
+        soup = BeautifulSoup(html, "html.parser")
+        src_list = [img.get("src") for img in soup.find_all("img")]
+        print("Start downloading...")
+        download_images_from_src_list(src_list, download_dir)
+        time.sleep(sleep_time_sec)
